9119aae8a58a1411aec48bb1a1a74b4cff4cdb1c
RipCurrentInteractive <RIPCURRENTINTERACTIVE@GMAIL.COM>
2023-12-20 02:19:10 -0600

    Commit Update Latest React JS Site

---
+++ precache-manifest.0e122f034f27226fd9a7436521668da7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fee236231b3ee36cb66822a816d5a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f37a34c462e3f77c0ecb",
+    "url": "/static/css/2.9d039907.chunk.css"
+  },
+  {
+    "revision": "bdeb5dbb463645ea0231",
+    "url": "/static/css/main.7e220ea7.chunk.css"
+  },
+  {
+    "revision": "f37a34c462e3f77c0ecb",
+    "url": "/static/js/2.17eb161a.chunk.js"
+  },
+  {
+    "revision": "02a1fa00d92ab19ed6e8",
+    "url": "/static/js/3.106f8bc7.chunk.js"
+  },
+  {
+    "revision": "bdeb5dbb463645ea0231",
+    "url": "/static/js/main.6a9f4ca8.chunk.js"
+  },
+  {
+    "revision": "fa9e233b3d25f113e4e2",
+    "url": "/static/js/runtime~main.180e7dc5.js"
+  },
+  {
+    "revision": "640520e4a76fb250fa4ff008ccf7cf97",
+    "url": "/static/media/backgroundvideo2.640520e4.mp4"
+  },
+  {
+    "revision": "6317c1f7a6fb8cadcc168908b63e3a60",
+    "url": "/static/media/companylogo.6317c1f7.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
