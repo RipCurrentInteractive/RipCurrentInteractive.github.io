d0f89d8790a4e6b54bee7a38e6f61a8ebfde5c94
RipCurrentInteractive <RIPCURRENTINTERACTIVE@GMAIL.COM>
2025-06-05 19:21:25 -0500

    update

---
+++ WaldropITTechServices/static/js/214.1e30328c.chunk.js
@@ -0,0 +1,20 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[214],{5214:(e,n,i)=>{i.r(n),i.d(n,{default:()=>p});var r=i(1319),t=i(4647),d=i(9281),a=i(3614),s=i(5639),c=i(9);const m=(0,c.Ay)("section")`
+  position: relative;
+  padding: 7.5rem 0 3rem;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 1024px) {
+    padding: 5.5rem 0 3rem;
+  }
+`,l=((0,c.Ay)("p")`
+  padding: 0.75rem 0 0.75rem;
+`,(0,c.Ay)("div")`
+  max-width: 570px;
+
+  @media only screen and (max-width: 768px) {
+    max-width: 100%;
+  }
+`);var o=i(579);const p=(0,d.C)()((e=>{let{title:n,content:i,button:d,t:c}=e;return(0,o.jsx)(m,{children:(0,o.jsx)(a.q7,{direction:"up",triggerOnce:!0,children:(0,o.jsx)(r.A,{justify:"center",align:"middle",children:(0,o.jsx)(l,{children:(0,o.jsx)(t.A,{lg:24,md:24,sm:24,xs:24,children:(0,o.jsx)(s.$,{color:"primary",href:"https://ulm.aeroadmin.com/AeroAdmin.exe",children:"Download AeroAdmin For Windows Here"})})})})})})}))}}]);
+//# sourceMappingURL=214.1e30328c.chunk.js.map
