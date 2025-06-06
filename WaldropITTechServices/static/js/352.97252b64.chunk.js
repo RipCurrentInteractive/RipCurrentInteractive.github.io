bd05d3629c8780c1014023ee46fda099a3226df1
RipCurrentInteractive <RIPCURRENTINTERACTIVE@GMAIL.COM>
2025-06-06 13:24:31 -0500

    update

---
+++ WaldropITTechServices/static/js/352.97252b64.chunk.js
@@ -0,0 +1,20 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[352],{5352:(e,t,r)=>{r.r(t),r.d(t,{default:()=>o});var s=r(1319),i=r(4647),n=r(9281),a=r(3614),d=r(5639),c=r(9);const m=(0,c.Ay)("section")`
+  position: relative;
+  padding: 7.5rem 0 3rem;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 1024px) {
+    padding: 5.5rem 0 3rem;
+  }
+`,l=((0,c.Ay)("p")`
+  padding: 0.75rem 0 0.75rem;
+`,(0,c.Ay)("div")`
+  max-width: 570px;
+
+  @media only screen and (max-width: 768px) {
+    max-width: 100%;
+  }
+`);var h=r(8609),p=r(579);const o=(0,n.C)()((e=>{let{title:t,content:r,button:n,t:c,id:o}=e;return(0,p.jsx)(m,{"aria-label":"payments",children:(0,p.jsx)(a.q7,{direction:"up",triggerOnce:!0,children:(0,p.jsx)(s.A,{justify:"center",align:"middle",children:(0,p.jsxs)(l,{children:[(0,p.jsx)(i.A,{lg:24,md:24,sm:24,xs:24,children:(0,p.jsx)(h.hE,{children:"Payments"})}),(0,p.jsx)(i.A,{lg:24,md:24,sm:24,xs:24,children:(0,p.jsx)(d.$,{color:"primary",href:"https://buy.stripe.com/test_cNifZj7et2XAbKG6p4aZi00",children:"Make A Payment (Less Than 30 Minutes) $50"})}),(0,p.jsx)(i.A,{lg:24,md:24,sm:24,xs:24,children:(0,p.jsx)(d.$,{color:"primary",href:"https://buy.stripe.com/test_5kQ7sNcyNcyabKG14KaZi01",children:"Make A Payment (More than 30 Minutes Less Than 1 Hour) $80 to $100"})}),(0,p.jsx)(i.A,{lg:24,md:24,sm:24,xs:24,children:(0,p.jsx)(d.$,{color:"primary",href:"https://buy.stripe.com/test_4gM9AVfKZ69M5mi9BgaZi02",children:"Make A Payment (More Than 1 Hour) $120 to $180"})})]})})})})}))}}]);
+//# sourceMappingURL=352.97252b64.chunk.js.map
